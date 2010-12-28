44dc602646a39c62bfc14422a4ef6cf95db67d97
Ted Dunning <ted.dunning@gmail.com>
2010-12-28 09:45:23 -0800

    ZOOKEEPER-965 - Added equals to facilitate testing and hashcode to match equals

---
+++ src/java/test/org/apache/zookeeper/MultiTransactionRecordTest.java
@@ -0,0 +1,11 @@
+package org.apache.zookeeper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tdunning
+ * Date: 12/28/10
+ * Time: 8:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MultiTransactionRecordTest {
+}
