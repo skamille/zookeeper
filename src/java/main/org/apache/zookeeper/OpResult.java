7b988de82fd6082d541a3247e2310639ae080884
Ted Dunning <ted.dunning@gmail.com>
2011-01-03 00:10:05 -0800

    ZOOKEEPER-965 - Added equals to response related classes.  Added response serialization test.

--- src/java/main/org/apache/zookeeper/OpResult.java
+++ src/java/main/org/apache/zookeeper/OpResult.java
@@ -1,5 +1,6 @@
 package org.apache.zookeeper;
 
+
 import org.apache.zookeeper.data.Stat;
 
 /**
@@ -27,11 +28,39 @@ public class OpResult {
         public String getName() {
             return name;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof OpResult)) return false;
+
+            CreateResult other = (CreateResult) o;
+            return getType() == other.getType() && name.equals(other.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return getType() * 35 + name.hashCode();
+        }
     }
 
     public static class DeleteResult extends OpResult {
         public DeleteResult() {
             super(ZooDefs.OpCode.delete);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof OpResult)) return false;
+
+            OpResult opResult = (OpResult) o;
+            return getType() == opResult.getType();
+        }
+
+        @Override
+        public int hashCode() {
+            return getType();
         }
     }
 
@@ -46,6 +75,20 @@ public class OpResult {
         public Stat getStat() {
             return stat;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof OpResult)) return false;
+
+            SetDataResult other = (SetDataResult) o;
+            return getType() == other.getType() && stat.getMzxid() == other.stat.getMzxid();
+        }
+
+        @Override
+        public int hashCode() {
+            return (int) (getType() * 35 + stat.getMzxid());
+        }
     }
 
     public static class CheckResult extends OpResult {
@@ -58,6 +101,20 @@ public class OpResult {
 
         public Stat getStat() {
             return stat;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof OpResult)) return false;
+
+            CheckResult other = (CheckResult) o;
+            return getType() == other.getType() && stat.getMzxid() == other.stat.getMzxid();
+        }
+
+        @Override
+        public int hashCode() {
+            return (int) (getType() * 35 + stat.getMzxid());
         }
     }
 }
