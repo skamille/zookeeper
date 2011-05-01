691b3b602e5867c8e3ace9f40a255f69379236da
Marshall McMullen <marshall@solidfire.com>
2011-05-01 13:53:48 -0600

    ZOOKEEPER-965 - Refactor sync_completion out of zookeeper_process
    Moved the gigantic "else" statement from zookeeper_process that
    processed sync_completions. This is because I will need it to
    process completions inside multi-op list of completions so it
    needs to be a standalone function that can be called from both
    places.
    
    Only real change was to move the logic that checked return code
    inside each case statement to instead be checked in the caller.
    Greatly simplified code with no functional change.

--- src/c/src/zookeeper.c
+++ src/c/src/zookeeper.c
@@ -1757,6 +1757,90 @@ completion_list_t *dequeue_completion(completion_head_t *list)
     return cptr;
 }
 
+static void process_sync_completion(int type,
+        struct sync_completion *sc,
+        struct iarchive *ia)
+{
+    switch(type) {
+        case COMPLETION_DATA: {
+            struct GetDataResponse res;
+            int len;
+            deserialize_GetDataResponse(ia, "reply", &res);
+            if (res.data.len <= sc->u.data.buff_len) {
+                len = res.data.len;
+            } else {
+                len = sc->u.data.buff_len;
+            }
+            sc->u.data.buff_len = len;
+            // check if len is negative
+            // just of NULL which is -1 int
+            if (len == -1) {
+                sc->u.data.buffer = NULL;
+            } else {
+                memcpy(sc->u.data.buffer, res.data.buff, len);
+            }
+            sc->u.data.stat = res.stat;
+            deallocate_GetDataResponse(&res);
+            break;
+        }
+        case COMPLETION_STAT: {
+            struct SetDataResponse res;
+            deserialize_SetDataResponse(ia, "reply", &res);
+            sc->u.stat = res.stat;
+            deallocate_SetDataResponse(&res);
+            break;
+        }
+        case COMPLETION_STRINGLIST: {
+            struct GetChildrenResponse res;
+            deserialize_GetChildrenResponse(ia, "reply", &res);
+            sc->u.strs2 = res.children;
+            /* We don't deallocate since we are passing it back */
+            // deallocate_GetChildrenResponse(&res);
+            break;
+        }
+        case COMPLETION_STRINGLIST_STAT: {
+            struct GetChildren2Response res;
+            deserialize_GetChildren2Response(ia, "reply", &res);
+            sc->u.strs_stat.strs2 = res.children;
+            sc->u.strs_stat.stat2 = res.stat;
+            /* We don't deallocate since we are passing it back */
+            // deallocate_GetChildren2Response(&res);
+            break;
+        }
+        case COMPLETION_STRING: {
+            struct CreateResponse res;
+            int len;
+            deserialize_CreateResponse(ia, "reply", &res);
+            len = strlen(res.path) + 1;
+            if (len > sc->u.str.str_len) {
+                len = sc->u.str.str_len;
+            }
+            if (len > 0) {
+                memcpy(sc->u.str.str, res.path, len - 1);
+                sc->u.str.str[len - 1] = '\0';
+            }
+            deallocate_CreateResponse(&res);
+            break;
+        }
+        case COMPLETION_ACLLIST: {
+            struct GetACLResponse res;
+            deserialize_GetACLResponse(ia, "reply", &res);
+            sc->u.acl.acl = res.acl;
+            sc->u.acl.stat = res.stat;
+            /* We don't deallocate since we are passing it back */
+            //deallocate_GetACLResponse(&res);
+            break;
+        }
+        case COMPLETION_VOID: {
+            break;
+        }
+        default: {
+            LOG_DEBUG(("UNKNOWN completion type"));
+            break;
+        }
+    }
+}
+
 
 /* handles async completion (both single- and multithreaded) */
 void process_completions(zhandle_t *zh)
@@ -2013,102 +2097,13 @@ int zookeeper_process(zhandle_t *zh, int events)
                 struct sync_completion
                         *sc = (struct sync_completion*)cptr->data;
                 sc->rc = rc;
-                switch(cptr->c.type) {
-                case COMPLETION_DATA:
-                    LOG_DEBUG(("Calling COMPLETION_DATA for xid=%#x rc=%d",
-                               cptr->xid, rc));
-                    if (rc==0) {
-                        struct GetDataResponse res;
-                        int len;
-                        deserialize_GetDataResponse(ia, "reply", &res);
-                        if (res.data.len <= sc->u.data.buff_len) {
-                            len = res.data.len;
-                        } else {
-                            len = sc->u.data.buff_len;
-                        }
-                        sc->u.data.buff_len = len;
-                        // check if len is negative
-                        // just of NULL which is -1 int
-                        if (len == -1) {
-                            sc->u.data.buffer = NULL;
-                        } else {
-                            memcpy(sc->u.data.buffer, res.data.buff, len);
-                        }
-                        sc->u.data.stat = res.stat;
-                        deallocate_GetDataResponse(&res);
-                    }
-                    break;
-                case COMPLETION_STAT:
-                    LOG_DEBUG(("Calling COMPLETION_STAT for xid=%#x rc=%d",
-                               cptr->xid, rc));
-                    if (rc == 0) {
-                        struct SetDataResponse res;
-                        deserialize_SetDataResponse(ia, "reply", &res);
-                        sc->u.stat = res.stat;
-                        deallocate_SetDataResponse(&res);
-                    }
-                    break;
-                case COMPLETION_STRINGLIST:
-                    LOG_DEBUG(("Calling COMPLETION_STRINGLIST for xid=%#x rc=%d",
-                               cptr->xid, rc));
-                    if (rc == 0) {
-                        struct GetChildrenResponse res;
-                        deserialize_GetChildrenResponse(ia, "reply", &res);
-                        sc->u.strs2 = res.children;
-                        /* We don't deallocate since we are passing it back */
-                        // deallocate_GetChildrenResponse(&res);
-                    }
-                    break;
-                case COMPLETION_STRINGLIST_STAT:
-                    LOG_DEBUG(("Calling COMPLETION_STRINGLIST_STAT for xid=%#x rc=%d",
-                               cptr->xid, rc));
-                    if (rc == 0) {
-                        struct GetChildren2Response res;
-                        deserialize_GetChildren2Response(ia, "reply", &res);
-                        sc->u.strs_stat.strs2 = res.children;
-                        sc->u.strs_stat.stat2 = res.stat;
-                        /* We don't deallocate since we are passing it back */
-                        // deallocate_GetChildren2Response(&res);
-                    }
-                    break;
-                case COMPLETION_STRING:
-                    LOG_DEBUG(("Calling COMPLETION_STRING for xid=%#x rc=%d",
-                               cptr->xid, rc));
-                    if (rc == 0) {
-                        struct CreateResponse res;
-                        int len;
-                        deserialize_CreateResponse(ia, "reply", &res);
-                        len = strlen(res.path) + 1;
-                        if (len > sc->u.str.str_len) {
-                            len = sc->u.str.str_len;
-                        }
-                        if (len > 0) {
-                            memcpy(sc->u.str.str, res.path, len - 1);
-                            sc->u.str.str[len - 1] = '\0';
-                        }
-                        deallocate_CreateResponse(&res);
-                    }
-                    break;
-                case COMPLETION_ACLLIST:
-                    LOG_DEBUG(("Calling COMPLETION_ACLLIST for xid=%#x rc=%d",
-                               cptr->xid, rc));
-                    if (rc == 0) {
-                        struct GetACLResponse res;
-                        deserialize_GetACLResponse(ia, "reply", &res);
-                        sc->u.acl.acl = res.acl;
-                        sc->u.acl.stat = res.stat;
-                        /* We don't deallocate since we are passing it back */
-                        //deallocate_GetACLResponse(&res);
-                    }
-                    break;
-                case COMPLETION_VOID:
-                    LOG_DEBUG(("Calling COMPLETION_VOID for xid=%#x rc=%d",
-                               cptr->xid, rc));
-                    break;
-                default:
-                    LOG_DEBUG(("UNKNOWN response type xid=%#x rc=%d",
-                               cptr->xid, rc));
-                    break;
+                
+                LOG_DEBUG(("Processing sync_completion with type=%d xid=%#x rc=%d",
+                    cptr->c.type, cptr->xid, sc->rc));
+
+                //Only process the sync completion of there were no errors
+                if (rc == 0) {
+                    process_sync_completion(cptr->c.type, sc, ia); 
                 }
                 notify_sync_completion(sc);
                 free_buffer(bptr);
